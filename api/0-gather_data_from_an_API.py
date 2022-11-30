c4282d335c9119b3e451e9dc1a0b654c4eca7a65
Ivan Hansen <ihk225@gmail.com>
2022-11-30 19:20:03 -0300

    Ex0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""
-    Fetch data from the corresponding API
+""" Fetch data from the corresponding API
     and show the results visually.
 
-    AIP used: https://jsonplaceholder.typicode.com/
-"""
-
+    API used: https://jsonplaceholder.typicode.com/ """
 from sys import argv
 import requests
-
 
 if __name__ == '__main__':
     emp_ID = int(argv[1])
@@ -16,10 +12,11 @@ if __name__ == '__main__':
                         timeout=10).json()
     user = requests.get(f"https://jsonplaceholder.typicode.com/users/{emp_ID}",
                         timeout=10).json()
+
     completed_tasks_list = [i for i in tasks if i['completed']
                             is True and i['userId'] == emp_ID]
     total_tasks = len([i for i in tasks if i['userId'] == emp_ID])
     print(f"Employee {user['name']} is done with tasks\
     ({len(completed_tasks_list)}/{total_tasks}):")
     for i in completed_tasks_list:
-        print("\t" + i['title'])
+        print("\t " + i['title'])
