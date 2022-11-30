70d640d62f5317303227fc3a6cd0330701edef68
Ivan Hansen <ihk225@gmail.com>
2022-11-30 20:45:41 -0300

    Ex0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,12 +11,10 @@ if __name__ == '__main__':
     tasks = requests.get("https://jsonplaceholder.typicode.com/todos").json()
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
                         .format(int(argv[1]))).json()
-    completed_tasks_list = [i for i in tasks if i.get('completed') and
+    completed_tasks = [i for i in tasks if i.get('completed') and
                             i.get('userId') == int(argv[1])]
     total_tasks = len([i for i in tasks if i['userId'] == int(argv[1])])
     print("Employee {} is done with tasks({}/{}):"
-          .format(str(user.get('name')),
-                  len(completed_tasks_list),
-                  total_tasks))
-    for i in completed_tasks_list:
+          .format(str(user.get('name')), len(completed_tasks), total_tasks))
+    for i in completed_tasks:
         print("\t {}".format(i.get('title')))
