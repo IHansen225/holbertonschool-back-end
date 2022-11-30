0a00c491ad5164fe11c4c9b715811fdd8510eb6a
Ivan Hansen <ihk225@gmail.com>
2022-11-30 20:27:16 -0300

    Ex0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 """
-Fetch data from the corresponding API
-and show the results visually.
-API used: https://jsonplaceholder.typicode.com/
+    Fetch data from the corresponding API
+    and show the results visually.
+    API used: https://jsonplaceholder.typicode.com/
 """
 import requests
 from sys import argv
 
 if __name__ == '__main__':
-    emp_ID = int(argv[1])
     tasks = requests.get("https://jsonplaceholder.typicode.com/todos",
                          timeout=10).json()
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                        .format(emp_ID),
+                        .format(int(argv[1])),
                         timeout=10).json()
     completed_tasks_list = [i for i in tasks if i.get('completed') and
                             i.get('userId') == emp_ID]
