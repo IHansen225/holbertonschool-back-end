e0bb9a6947fae4522341b66f5373760646e6e4d4
Ivan Hansen <ihk225@gmail.com>
2022-11-30 19:05:31 -0300

    Ex0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-""" Get data from API server """
+"""
+    Get data from API server
+    https://jsonplaceholder.typicode.com
+"""
 from sys import argv
 import requests
 
 
-emp_ID = int(argv[1])
-tasks = requests.get("https://jsonplaceholder.typicode.com/todos",
-                    timeout=10).json()
-user = requests.get(f"https://jsonplaceholder.typicode.com/users/{emp_ID}",
-                    timeout=10).json()
-completed_tasks_list = [i for i in tasks if i['completed']
-                        is True and i['userId'] == emp_ID]
-total_tasks = len([i for i in tasks if i['userId'] == emp_ID])
-print(f"Employee {user['name']} is done with tasks\
-({len(completed_tasks_list)}/{total_tasks}):")
-for i in completed_tasks_list:
-    print("\t" + i['title'])
+if __name__ == '__main__':
+    emp_ID = int(argv[1])
+    tasks = requests.get("https://jsonplaceholder.typicode.com/todos",
+                        timeout=10).json()
+    user = requests.get(f"https://jsonplaceholder.typicode.com/users/{emp_ID}",
+                        timeout=10).json()
+    completed_tasks_list = [i for i in tasks if i['completed']
+                            is True and i['userId'] == emp_ID]
+    total_tasks = len([i for i in tasks if i['userId'] == emp_ID])
+    print(f"Employee {user['name']} is done with tasks\
+    ({len(completed_tasks_list)}/{total_tasks}):")
+    for i in completed_tasks_list:
+        print("\t" + i['title'])
