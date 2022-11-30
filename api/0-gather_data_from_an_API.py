967a5d8b4679feefaadccee29db11e804025f0dc
Ivan Hansen <ihk225@gmail.com>
2022-11-30 19:01:36 -0300

    Ex0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 from sys import argv
-import json
 import requests
 
 emp_ID = int(argv[1])
-response = requests.get(f'https://jsonplaceholder.typicode.com/todos/{emp_ID}', verify=False, timeout=10)
-print(response.json())
+tasks = requests.get("https://jsonplaceholder.typicode.com/todos",
+                    timeout=10).json()
+user = requests.get(f"https://jsonplaceholder.typicode.com/users/{emp_ID}",
+                    timeout=10).json()
+completed_tasks_list = [i for i in tasks if i['completed']
+                        is True and i['userId'] == emp_ID]
+total_tasks = len([i for i in tasks if i['userId'] == emp_ID])
+print(f"Employee {user['name']} is done with tasks\
+({len(completed_tasks_list)}/{total_tasks}):")
+for i in completed_tasks_list:
+    print("\t" + i['title'])
