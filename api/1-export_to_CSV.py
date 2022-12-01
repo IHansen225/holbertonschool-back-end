65e417bfacd8a1b0dcdda6825f71ba303c230c5e
Ivan Hansen <ihk225@gmail.com>
2022-11-30 21:12:03 -0300

    Ex1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -6,16 +6,16 @@
 """
 import requests
 from sys import argv
-import pandas as pd
 
 if __name__ == '__main__':
     tasks = requests.get("https://jsonplaceholder.typicode.com/todos").json()
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
                         .format(int(argv[1]))).json()
-    tlst = []
+    csvs = ""
+    uname = user.get('username')
+    uid = str(argv[1])
     for i in tasks:
         if i.get('userId') == int(argv[1]):
-            tlst.append({'UID': argv[1], 'Uname': user.get('username'),
-                 'status': i.get('completed'), 'title': i.get('title')})
-    df = pd.DataFrame(tlst)
-    df.to_csv('USER_ID.csv', index=False, header=False)
+            csvs += ("{}, {}, {}, {}\n"
+            .format(uid, uname, i.get('completed'), i.get('title')))
+    print(csvs)
