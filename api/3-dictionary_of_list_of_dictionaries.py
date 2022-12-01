45daadcff9e5df8dad4c180dc1c79a134942c95e
Ivan Hansen <ihk225@gmail.com>
2022-11-30 22:55:52 -0300

    Ex3

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Fetch data from the corresponding API
+    and show the results visually.
+    API used: https://jsonplaceholder.typicode.com/
+"""
+import json
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    tasks = requests.get("https://jsonplaceholder.typicode.com/todos").json()
+    user = requests.get("https://jsonplaceholder.typicode.com/users/").json()
+    all_dump = dict()
+    for u in user:
+        all_dump["{}".format(u.get('id'))] = [{"task": t.get('title'),
+                                               "completed": t.get('completed'),
+                                               "username": u.get('username')} for t in tasks]
+    with open("todo_all_employees.json", "w", encoding="UTF-8") as f:
+        json.dump(all_dump, f)
